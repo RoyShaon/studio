7d4ab6bf70e92353787f3d820a3402d07a814d42
Roy Shaon <dev.royshaon@gmail.com>
2025-10-05 17:23:33 +0000

    11:22:36 PM: Failed during stage 'building site': Build script returned

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This rewrite configuration is a workaround to prevent Netlify and other
-  // hosting providers from serving the `index.html` file from the `public`
-  // directory, which was created by Firebase. By rewriting these paths to
-  // a non-existent destination with a 404 status, we ensure that the
-  // Next.js application is always served as the main entry point.
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/index.html',
-          destination: '/:path*',
-          has: [],
-        },
-        {
-          source: '/404.html',
-          destination: '/:path*',
-          has: [],
-        },
-      ],
-    };
-  },
+  // Your Next.js configuration options can go here.
+  // For now, we'll keep it simple to ensure the build passes.
 };
 
 export default nextConfig;
