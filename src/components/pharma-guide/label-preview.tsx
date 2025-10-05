04463e2111d5b2d0ab31bb9bf9904e46c45330df
Roy Shaon <dev.royshaon@gmail.com>
2025-10-05 13:43:55 +0000

    "ঔষধ সেবনের আগে শিশিটিকে হাতের তালুর উপরে দূর হতে সজোরে থেমে থেমে ১০ বার

--- src/components/pharma-guide/label-preview.tsx
+++ src/components/pharma-guide/label-preview.tsx
@@ -76,7 +76,7 @@ export default function LabelPreview({
     
     return (
       <div 
-        className="text-gray-800 text-justify text-lg leading-snug"
+        className="text-gray-800 text-justify text-[18.6px] leading-snug"
         dangerouslySetInnerHTML={{ __html: processedInstruction.replace(/\n/g, '<br>') }} 
       />
     );
