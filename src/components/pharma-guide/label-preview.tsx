4fb1fa0dbcaed62296002969b6869466c71ad599
Roy Shaon <dev.royshaon@gmail.com>
2025-10-05 13:11:07 +0000

    To get advice, text this underlined line that contains the earner line.

--- src/components/pharma-guide/label-preview.tsx
+++ src/components/pharma-guide/label-preview.tsx
@@ -122,7 +122,7 @@ export default function LabelPreview({
         {renderInstruction()}
 
         <div className="mt-3 text-center">
-          <h3 className="text-base font-bold text-red-700 mb-0.5 border-b-2 border-gray-800 inline-block pb-1">পরামর্শ</h3>
+          <h3 className="text-base font-bold text-red-700 mb-0.5 inline-block pb-1">পরামর্শ</h3>
           <ul
             className="advice-list text-gray-800 text-xs pl-0 list-none text-left"
             dangerouslySetInnerHTML={{ __html: counselingPoints }}
@@ -142,5 +142,6 @@ export default function LabelPreview({
     </div>
   );
 }
+
 
 
