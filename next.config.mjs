7ded54126ee3427533f6e4549e43395ca95377e6
Roy Shaon <dev.royshaon@gmail.com>
2025-10-05 17:20:19 +0000

    Please arrange for the application to be opened directly from my site wi

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  // This rewrite configuration is a workaround to prevent Netlify and other
+  // hosting providers from serving the `index.html` file from the `public`
+  // directory, which was created by Firebase. By rewriting these paths to
+  // a non-existent destination with a 404 status, we ensure that the
+  // Next.js application is always served as the main entry point.
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/index.html',
+          destination: '/:path*',
+          has: [],
+        },
+        {
+          source: '/404.html',
+          destination: '/:path*',
+          has: [],
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
