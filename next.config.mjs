7aac1d55cb58a2b1fcb06716a7cb231cb2a9bbee
Roy Shaon <dev.royshaon@gmail.com>
2025-10-06 08:41:30 +0000

    > nextn@0.1.0 build > next build
     ⚠ Invalid next.config.mjs options det

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        allowedDevOrigins: ["*.cloudworkstations.dev"],
-    },
-};
-
-export default nextConfig;
