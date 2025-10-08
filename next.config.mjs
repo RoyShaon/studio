ac99f1cc99b01e49380f07ada957181c1010aa58
Roy Shaon <dev.royshaon@gmail.com>
2025-10-08 18:09:46 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-import withPWAInit from '@ducanh2912/next-pwa';
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
-
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
