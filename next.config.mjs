c1dbd9d2d4379e53514a2e54be416d8dea8c2c93
Roy Shaon <dev.royshaon@gmail.com>
2025-10-08 18:07:41 +0000

    Try fixing this error: `Runtime ChunkLoadError: Loading chunk app/page f

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-import withPWAInit from '@ducanh2912/next-pwa';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // Your Next.js config
 };
 
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  // add more options here
-});
-
-export default withPWA(nextConfig);
+export default nextConfig;
