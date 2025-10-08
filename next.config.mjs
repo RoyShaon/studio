1c6a988e9e4d702a9c586af92bf013e637c1102c
Roy Shaon <dev.royshaon@gmail.com>
2025-10-08 18:08:22 +0000

    > nextn@0.1.0 build > next build
     ⚠ Invalid next.config.mjs options det

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
 const nextConfig = {
-  // Your Next.js config
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
